ab990d84d06ee0bad7319e6da37f0ebc91bfb362
deviprajwala <deviprajwala3@gmail.com>
2021-04-22 16:56:13 +0530

    mean adjusted matrix

---
+++ item_based.py
@@ -0,0 +1,19 @@
+#user based recommnder system
+import pandas as pd 
+import math
+rating = pd.read_csv(r"C:\\Users\\Devi Prajwala B S\\Downloads\\recommenders\\recommender_systems\\item_rating.csv")
+#rating.iat[1,4] = rating.iat[1,4].astype(float)
+#no_of_attributes = input( " Enter the number of attributes " )
+
+mean_of_each_users = rating.mean(axis = 1)
+#print(rating)
+
+def mean_adjusted_matrix( no_of_attributes, no_of_users ):
+    for i in range(no_of_users):
+        for j in range(1,no_of_attributes+1):
+            rating.loc[i].iat[j] = float(rating.loc[i].iat[j]) - mean_of_each_users[i]
+            #print(i,j)
+    
+    print(rating)
+
+mean_adjusted_matrix(5,4)
